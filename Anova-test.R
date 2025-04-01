7853fe1447ab80650103ee239a41d4cb5b9d17ae
nyasiaargrow <nyargrow@claflin.edu>
2025-04-01 10:15:23 -0400

    Add files via upload

--- Anova-test.R
+++ Anova-test.R
@@ -1,4 +1,4 @@
-#Author: Nyasia Argrow, Date: 3/27/2025, Purpose: Perform ANOVA Test.
+#Author: Nyasia Argrow, Date: 3/27/2025, 4/1/2025 Purpose: Perform ANOVA Test.
 
 #Load a library dplyr
 library(dplyr)
@@ -7,6 +7,22 @@ library(dplyr)
 PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poisons.csv"
 
 #Read the CSV file from above link
-df <- read.csv(PATH,row.names=NULL)
+df <- read.csv(PATH)
 
-df[,c(2:4)]
+#Load library ggplot
+library(ggplot2)
+
+#Plot your data using ggplot2
+ggplot(df, aes(x = poison, y = time, fill = poison)) + geom_boxplot() + geom_jitter(shape = 15, color = "steelblue", position = position_jitter(0.21)) + theme_classic()
+
+#Apply the ANOVA function
+anova_one_way <- aov(time~poison, data = df) 
+summary(anova_one_way)
+
+            Df Sum Sq Mean Sq F value   Pr(>F)    
+poison       1 0.9316  0.9316   20.67 3.96e-05 ***
+Residuals   46 2.0735  0.0451                     
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+#There is a significant difference.
